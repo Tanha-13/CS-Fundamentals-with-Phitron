ffae1dd9daca3d104e147b1f161e0eec484b8205
Tanha <nusrattanha897@gmail.com>
2024-10-16 21:35:45 +0600

    solved 4 problems fromcontest sheet 01

---
+++ Codeforces-group-contests/contest#1/B_Memo-momo.c
@@ -0,0 +1,52 @@
+/**
+ * Memo and Momo are playing a game. Memo will choose a positive number a
+, and Momo will choose a positive number b
+.
+
+Your task is to tell them who will win according to the following rules:
+
+If both a
+ and b
+ are divisible by k
+, both of them win and you should print "Both".
+If a
+ is divisible by k
+ but b
+ isn't, Memo wins and you should print "Memo".
+If b
+ is divisible by k
+ but a
+ isn't, Momo wins and you should print "Momo".
+If both a
+ and b
+ are not divisible by k
+, no one wins and you should print "No One".
+Input
+Only one line containing three positive numbers a
+, b
+ and k
+ (1≤a,b,k≤1018
+).
+
+Output
+Print the answer as described in the statement.
+ */
+#include<stdio.h>
+int main(){
+
+    long long int a,b,k;
+    scanf("%lld %lld %lld", &a,&b,&k);
+    if(a%k == 0 && b%k == 0){
+        printf("Both");
+    }
+    else if(a%k == 0 && b%k != 0){
+        printf("Memo");
+    }
+    else if(a%k != 0 && b%k == 0){
+        printf("Momo");
+    }
+    else if(a%k != 0 && a%k != 0){
+        printf("No One");
+    }
+    return 0;
+}
